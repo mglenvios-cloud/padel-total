e41dad1bc9fe085b2c69729a76ab5960b495c71c
Claudio <claudio@example.com>
2026-07-24 09:24:23 -0300

    fix: deshabilitar completamente Service Worker al inicio para desbloquear peticiones de red

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,7 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
       .then(() => self.registration.unregister())
   );
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
 });
