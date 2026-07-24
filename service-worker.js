27b51725debe6b41c64c8cd07aaef22c34222e83
Claudio <claudio@example.com>
2026-07-24 09:22:53 -0300

    fix: desactivar y desregistrar Service Worker defectuoso que bloqueaba las peticiones de red

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,10 @@
-const CACHE_NAME = 'padel-pro-evo-v1';
-const ASSETS = [
-  '/game3d.html',
-  '/js/game3d.js',
-  '/js/renderer3d.js',
-  '/js/physics3d.js',
-  '/js/ai3d.js',
-  '/js/audio.js'
-];
-
-self.addEventListener('install', (e) => {
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
